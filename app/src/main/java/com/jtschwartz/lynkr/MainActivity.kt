65f027f8a8988ac6a801f3e2a518dea044de2f49
Jacob Schwartz <jtschwartz98@gmail.com>
2019-11-20 02:19:32 -0500

    Communication between Main and Settings Ability to pull data on bonded devices

--- app/src/main/java/com/jtschwartz/lynkr/MainActivity.kt
+++ app/src/main/java/com/jtschwartz/lynkr/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.jtschwartz.lynkr
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+	
+	var btDevice: BluetoothDevice? = null
+	var btAddress: String? = null
+	val OPEN_SETTINGS = 1
+	
+	companion object {
+		val EXTRA_ADDRESS: String = "Device_Address"
+	}
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		
+		curBtDevice.text = btAddress ?: "No Chosen Bluetooth Device"
 	}
 	
 	fun btnCamera(view: View) {
@@ -51,6 +65,23 @@ class MainActivity : AppCompatActivity() {
 	fun btnSettings(view: View) {
 		println("Settings")
 		val openSettings = Intent(this, SettingsActivity::class.java)
-		startActivity(openSettings)
+		openSettings.putExtra(EXTRA_ADDRESS, btAddress)
+		startActivityForResult(openSettings, OPEN_SETTINGS)
+	}
+	
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		if (requestCode == OPEN_SETTINGS) {
+			if (resultCode == Activity.RESULT_OK) {
+				val returnedDevice = data?.getStringExtra(SettingsActivity.EXTRA_ADDRESS)
+				println("Got Device: $returnedDevice")
+				curBtDevice.text = returnedDevice.toString()
+				
+				if (returnedDevice != null) {
+					btAddress = returnedDevice.toString()
+					btDevice = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(returnedDevice.toString())
+				}
+			}
+		}
 	}
 }
--- app/src/main/java/com/jtschwartz/lynkr/SettingsActivity.kt
+++ app/src/main/java/com/jtschwartz/lynkr/SettingsActivity.kt
@@ -1,17 +1,84 @@
 package com.jtschwartz.lynkr
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_settings.*
+import org.jetbrains.anko.toast
 
 class SettingsActivity : AppCompatActivity() {
+	
+	var bluetoothAdapter: BluetoothAdapter? = null
+	lateinit var pairedDevices: Set<BluetoothDevice>
+	val REQUEST_ENABLE_BLUETOOTH = 1
+	var btDevice: BluetoothDevice? = null
+	
+	companion object {
+		const val EXTRA_ADDRESS: String = "BT_Device"
+	}
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_settings)
+		
+		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+		if (bluetoothAdapter == null) {
+			toast("Device does not support Bluetooth")
+			return
+		} else if (!bluetoothAdapter!!.isEnabled){
+			val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+			startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+		}
+		
+		refreshBluetoothList()
+	}
+	
+	fun refreshBluetoothList(view: View? = null) {
+		pairedDevices = bluetoothAdapter!!.bondedDevices
+		val btDeviceList: ArrayList<BluetoothDevice> = ArrayList()
+		
+		if (pairedDevices.isNotEmpty()) {
+			for (device: BluetoothDevice in pairedDevices) {
+				btDeviceList.add(device)
+			}
+		} else {
+			toast("No paired bluetooth devices found")
+		}
+		
+		val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, btDeviceList)
+		settings_devices.adapter = adapter
+		settings_devices.onItemClickListener = AdapterView.OnItemClickListener{_, _, pos, _ ->
+			btDevice = btDeviceList[pos]
+		}
 	}
 	
 	fun navigateHome(view: View) {
+		val result = Intent()
+		result.putExtra(EXTRA_ADDRESS, btDevice.toString())
+		setResult(Activity.RESULT_OK, result)
+		
 		finish()
+	}
+	
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+			if (resultCode == Activity.RESULT_OK) {
+				if (bluetoothAdapter!!.isEnabled) {
+					toast("Bluetooth has been enabled")
+				} else {
+					toast("Bluetooth has been disabled")
+				}
+			} else if (resultCode == Activity.RESULT_CANCELED) {
+				toast("Bluetooth enabling has been cancelled")
+			}
+		}
 	}
 }
